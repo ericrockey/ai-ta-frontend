f994000d278535635ba1a5ab1181c4bc850d6691
ericrockey <eric@wetransfer.com>
2023-08-20 17:27:24 -0400

    fix: remove DATABASE_URL

--- src/env.mjs
+++ src/env.mjs
@@ -7,7 +7,6 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z.string().url(),
     NODE_ENV: z.enum(['development', 'test', 'production']),
     SUPABASE_URL: z.string().url(),
     SUPABASE_SECRET: z.string().min(1),
@@ -47,7 +46,6 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     SUPABASE_URL: process.env.SUPABASE_URL,
     SUPABASE_SECRET: process.env.SUPABASE_SECRET,
