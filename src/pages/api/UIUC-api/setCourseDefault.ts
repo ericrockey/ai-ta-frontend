500fe991a1835ab4530befc0f8f627dc40545c71
ericrockey <eric@wetransfer.com>
2023-08-21 14:48:32 -0400

    fix: add setDefault handler

---
+++ src/pages/api/UIUC-api/setCourseDefault.ts
@@ -0,0 +1,34 @@
+import { kv } from '@vercel/kv'
+import { NextResponse } from 'next/server'
+import { CourseMetadata } from '~/types/courseMetadata'
+
+export const runtime = 'edge'
+
+const setCourseDefault = async (req: any, res: any) => {
+  const course_name = req.nextUrl.searchParams.get('course_name')
+  const is_default = req.nextUrl.searchParams.get('is_default')
+
+  try {
+    const course_metadata = (await kv.get(
+      course_name + '_metadata',
+    )) as CourseMetadata
+
+    if (!course_metadata) {
+      res.status(500).json({ success: false })
+      return
+    }
+
+    const updated_course_metadata: CourseMetadata = {
+      ...course_metadata,
+      is_default, // ONLY CHANGE
+    }
+
+    await kv.set(course_name + '_metadata', updated_course_metadata)
+    return NextResponse.json({ success: true })
+  } catch (error) {
+    console.log(error)
+    console.log('removeUserFromCourse FAILURE')
+    return NextResponse.json({ success: false })
+  }
+}
+export default setCourseDefault
