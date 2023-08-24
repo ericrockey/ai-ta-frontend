0c6f9cef6854d71c5d1cdd1f6043486311a9efa8
ericrockey <eric@wetransfer.com>
2023-08-24 09:22:54 -0400

    fix: fix sidebar button position

--- src/components/Sidebar/components/OpenCloseButton.tsx
+++ src/components/Sidebar/components/OpenCloseButton.tsx
@@ -1,4 +1,6 @@
 import { IconArrowBarLeft, IconArrowBarRight } from '@tabler/icons-react'
+import classnames from 'classnames';
+import styles from './OpenCloseButton.module.scss'
 
 interface Props {
   onClick: any
@@ -9,11 +11,11 @@ export const CloseSidebarButton = ({ onClick, side }: Props) => {
   return (
     <>
       <button
-        className={`fixed top-5 ${
+        className={classnames(styles.sidebarButton, ` ${
           side === 'right' ? 'right-[270px]' : 'left-[270px]'
         } z-50 h-7 w-7 hover:text-gray-400 dark:text-white dark:hover:text-gray-300 sm:top-0.5 sm:${
           side === 'right' ? 'right-[270px]' : 'left-[270px]'
-        } sm:h-8 sm:w-8 sm:text-neutral-700`}
+        } sm:h-8 sm:w-8 sm:text-neutral-700`)}
         onClick={onClick}
       >
         {side === 'right' ? <IconArrowBarRight /> : <IconArrowBarLeft />}
@@ -29,11 +31,11 @@ export const CloseSidebarButton = ({ onClick, side }: Props) => {
 export const OpenSidebarButton = ({ onClick, side }: Props) => {
   return (
     <button
-      className={`fixed top-2.5 ${
+      className={classnames(styles.sidebarButton, `fixed top-2.5 ${
         side === 'right' ? 'right-2' : 'left-2'
       } z-50 h-7 w-7 text-white hover:text-gray-400 dark:text-white dark:hover:text-gray-300 sm:top-0.5 sm:${
         side === 'right' ? 'right-2' : 'left-2'
-      } sm:h-8 sm:w-8 sm:text-neutral-700`}
+      } sm:h-8 sm:w-8 sm:text-neutral-700`)}
       onClick={onClick}
     >
       {side === 'right' ? <IconArrowBarLeft /> : <IconArrowBarRight />}
